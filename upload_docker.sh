11e235830e9cd138270093aae671bd4d7633e1a7
Patrick Coutinho <patrick@istyle.com.br>
2022-06-03 21:21:52 -0300

    added steps to upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
 
-# Assumes that an image is built via `run_docker.sh`
+image=ml-microservice-k8s-app
+dockerpath=patrickcoutinho/$image
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+echo $DOCKER_HUB_PASS | docker login -u=patrickcoutinho --password-stdin
+docker image tag $image:latest $dockerpath:latest
 
-# Step 2:  
-# Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
-
-# Step 3:
-# Push image to a docker repository
+docker push $dockerpath:latest
